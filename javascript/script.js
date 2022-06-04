a39cf2583125b8083529036ee7be0c2e435a6e27
ChiragBangera <chiragbangera468@gmail.com>
2022-06-04 17:06:55 +0530

    sdfv

--- javascript/script.js
+++ javascript/script.js
@@ -8,21 +8,33 @@ let favanimal = "Elephant";
 
 // indeces and lengths
 console.log(favanimal.length)
-console.log(username+" "+favanimal)
+console.log(username + " " + favanimal)
 console.log(username.toUpperCase())
 console.log(favanimal.slice(-1))
-console.log(username.replace("danny","Chirag"))
+console.log(username.replace("danny", "Chirag"))
 
 let networth = "20 Million Dollors";
 
 // Random Numers Math objects
 console.log(Math.PI)
-console.log(Math.floor(Math.random()*6)+20)
+console.log(Math.floor(Math.random() * 6) + 20)
+console.log(`${username} ${favanimal}`)
 
+let rating = Math.floor(Math.random() * 5)
+if (rating === 3) {
+    console.log("Average Rating");
+} else if (rating === 4) {
+    console.log("Above Average");
+} else {
+    console.log("No Rating Found");
+}
 
-
-
-
+let password = prompt("Enter A Password")
+if (password.length>6 && password.indexOf(" ")=== -1){
+    console.log("Password Acceptale");
+}else if (!password){
+    password = prompt("Enter A Password")
+}
 
 
 
