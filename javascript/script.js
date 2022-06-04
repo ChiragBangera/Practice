be86ebd45472a840b13995e1efb0e159430243b1
ChiragBangera <chiragbangera468@gmail.com>
2022-06-04 12:23:47 +0530

    set

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,30 @@
 let numhens = 5;
-console.log(numhens+1)
+console.log(numhens + 1)
+
+
+let username = "danny is a good boy";
+
+let favanimal = "Elephant";
+
+// indeces and lengths
+console.log(favanimal.length)
+console.log(username+" "+favanimal)
+console.log(username.toUpperCase())
+console.log(favanimal.slice(-1))
+console.log(username.replace("danny","Chirag"))
+
+let networth = "20 Million Dollors";
+
+// Random Numers Math objects
+console.log(Math.PI)
+console.log(Math.floor(Math.random()*6)+20)
+
+
+
+
+
+
+
+
+
+
