6def0465567f547b98226ebcba97fdbd54f4fe08
ChiragBangera <chiragbangera468@gmail.com>
2022-06-06 23:20:21 +0530

    set

--- Python/Flipkart_Desc_scraper.py
+++ Python/Flipkart_Desc_scraper.py
@@ -9,7 +9,7 @@ df1 = pd.read_csv(".csv")
 
 url1 = "https://www.flipkart.com/rage-coffee-100-gms-irish-hazelnut-flavour-premium-arabica-instant/p/itmcf8c39c70131f?pid=CFEFNAGUHTPFMEJZ&lid=LSTCFEFNAGUHTPFMEJZKKSIHZ&marketplace=FLIPKART&q=rage+coffee&store=eat%2Fdui&srno=s_1_3&otracker=search&otracker1=search&fm=Search&iid=57739a88-d217-494a-8c5d-4ed434ac6b2b.CFEFNAGUHTPFMEJZ.SEARCH&ppt=sp&ppn=sp&ssid=uravxh3a4g0000001654535955038&qH=3c18955655658f0c"
 results = []
-for url in df1{'Product Links'}:
+for url in df1['Product Links']:
     HEADERS = ({'User-Agent':
                    'Mozilla/5.0 (X11; Linux x86_64)AppleWebKit/537.36 (KHTML, like Gecko)Chrome/44.0.2403.157 Safari/537.36',
                                    'Accept-Language': 'en-US, en;q=0.5'})
@@ -22,4 +22,5 @@ for url in df1{'Product Links'}:
     for item in tables[1].find_all('tr'):
         data1[item.td.text] = item.ul.text
     results.append(data1)
-df = df = pd.DataFrame(results)
+df = pd.DataFrame(results)
+df.to_csv("Flipkart_Desc_data")
