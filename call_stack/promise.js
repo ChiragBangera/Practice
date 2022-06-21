5a919f2636bdb6b34d64717f70da97ce7832881f
ChiragBangera <chiragbangera468@gmail.com>
2022-06-21 21:35:30 +0530

    set

---
+++ call_stack/promise.js
@@ -0,0 +1,47 @@
+const fake_request = (url)=>{
+    return new Promise((resolve,reject)=>{
+        const rand = Math.random();
+        setTimeout(()=>{
+            if (rand<.7){
+                resolve("Your fake Data")
+            }
+            reject("ERROR")
+        },1500)
+    })
+}
+
+
+fake_request('/dogs/1')
+    .then((data) => {
+        console.log("Done with the data", data);
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+
+const changeColor = (color,time)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            document.body.style.backgroundColor = color;
+            resolve()
+        },time)
+    })
+}
+
+
+changeColor('red',1000)
+    .then(()=>{
+        return changeColor('orange',1000)
+    })
+    .then(()=>{
+        return changeColor('grey',1000)
+    })
+    .then(()=>{
+        return changeColor('deeppink',1000)
+    })
+    .then(()=>{
+        return changeColor('yellow',1000)
+    })
+    .catch(()=>{
+        console.log('Error')
+    })
