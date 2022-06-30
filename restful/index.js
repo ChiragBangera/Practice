4d2ebe9036e94e47830bfc486cd1a22a2955fda2
ChiragBangera <chiragbangera468@gmail.com>
2022-06-30 22:18:32 +0530

    uu

--- restful/index.js
+++ restful/index.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const app = express()
-
 app.use(express.urlencoded({extended:true}))
 app.use(express.json({extended:true}))
+app.set('view engine', 'ejs')
+
+const comments = [
+    {
+        id:1,  
+        username:"Chirag",
+        comment:"I will be The best"
+    },
+    {
+        id:2,
+        username:"Janmitha",
+        comment:"I am The best after Chirag"
+    },
+    {
+        id:3,
+        username:"Rajeshwari",
+        comment:"I am the best"
+    },
+    {
+        id:4,
+        username:"Chandrahas",
+        comment:"I am the best after Rajeshwari"
+    },
+
+]
 
 app.get('/tacos',(req,res)=>{
     res.send('GET/ tacos')
@@ -11,6 +35,26 @@ app.get('/tacos',(req,res)=>{
 app.post('/tacos',(req,res)=>{
     console.log(req.body)
     res.send('post/ tacos')
+})
+
+app.get('/comments',(req,res)=>{
+    res.render("index.ejs",{comments})
+})
+
+app.get('/comments/new',(req,res)=>{
+    res.render('new.ejs')
+})
+
+app.post('/comments',(req,res)=>{
+    const {username,comment} = req.body
+    comments.push({username,comment})
+    res.redirect('/comments')
+})
+
+app.get('/comments/:id',(req,res)=>{
+    const {id}=req.params
+    const comment = comments.find(c=>c.id===parseInt(id))
+    res.render('show.ejs',{comment})
 })
 
 
