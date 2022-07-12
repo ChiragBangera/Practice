56c80073b05a75d07474c8d73689e909cfb1f1bc
ChiragBangera <chiragbangera468@gmail.com>
2022-07-13 01:01:45 +0530

    gn

--- errorhandlers/fullcrud.js
+++ errorhandlers/fullcrud.js
@@ -7,79 +7,98 @@ const mongoose = require('mongoose')
 const DB = "mongodb+srv://chiragbangera:chiragadarsh@cluster1.c3lu4j0.mongodb.net/stockStatus?retryWrites=true&w=majority"
 mongoose.connect(DB,
     {
-        useNewUrlParser:true,
-        useUnifiedTopology:true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     })
-    .then(()=>{
+    .then(() => {
         console.log('Connected to MongoDB')
     })
-    .catch((e)=>{
+    .catch((e) => {
         console.log('MongoDB Not Connected')
         console.log(e)
     })
 
-const cat = ['shirt','t-shirt','jeans','underwear']
+const cat = ['shirt', 't-shirt', 'jeans', 'underwear']
 
 
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.use(methodoverride('_method'))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 // main
-app.get('/products',async (req,res)=>{
-    const {category} = req.query
-    if(category){
-        const products = await Product.find({category})
-        res.render('products/index', {products, category})
-    }else{
-        const products = await Product.find({})
-    res.render('products/index', {products,category:'All'})
+app.get('/products', async (req, res,next) => {
+    try {
+        const { category } = req.query
+        if (category) {
+            const products = await Product.find({ category })
+            res.render('products/index', { products, category })
+        } else {
+            const products = await Product.find({})
+            res.render('products/index', { products, category: 'All' })
+        }
+    }catch (err){
+        next(err)
     }
-    
 })
 
 //  form
-app.get('/products/new',(req,res)=>{
-    res.render('products/new',{cat})
+app.get('/products/new', (req, res) => {
+    res.render('products/new', { cat })
 })
 
-app.get('/products/:id/edit',async (req,res)=>{
-    const {id} = req.params
-    const product  = await Product.findById(id)
-    res.render('products/edit',{product,cat})
+app.get('/products/:id/edit', async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findById(id)
+        res.render('products/edit', { product, cat })
+    } catch (err) {
+        next(err)
+    }
 })
 
 
 // Products Page Route
-app.post('/products',async (req,res)=>{
-    const newProduct = new Product(req.body)
-    await newProduct.save()
-    res.redirect(`/products/${newProduct._id}`)
+app.post('/products', async (req, res, next) => {
+    try {
+        const newProduct = new Product(req.body)
+        await newProduct.save()
+        res.redirect(`/products/${newProduct._id}`)
+    } catch (err) {
+        next(err)
+    }
 })
 
 // product Show Page
-app.get('/products/:id',async (req,res,next)=>{
-    const {id} = req.params
-    const product  = await Product.findById(id)
-    if(!product){
-       return next(new AppError("Not Found",500)) 
+app.get('/products/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findById(id)
+        if (!product) {
+           throw new AppError("Not Found", 500)
+        }
+        res.render('products/show', { product })
+    } catch (err) {
+        next(err)
     }
-    res.render('products/show',{product})
- })
+})
 
 //  Products show page edit route
-app.put('/products/:id',async (req,res)=>{
-    const {id} = req.params
-    const updateProduct = await Product.findByIdAndUpdate(id,req.body,{runValidators:true, new:true})
-    await updateProduct.save()
-    res.redirect(`/products/${updateProduct._id}`)
+app.put('/products/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const updateProduct = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+        await updateProduct.save()
+        res.redirect(`/products/${updateProduct._id}`)
+    } catch (err) {
+        next(err)
+    }
 })
 
 // delete
-app.delete('/products/:id',async (req,res)=>{
-    const {id} = req.params
-    const product  = await Product.findByIdAndDelete(id)
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findByIdAndDelete(id)
     res.redirect('/products')
 })
 
@@ -92,8 +111,8 @@ app.delete('/products/:id',async (req,res)=>{
 
 
 
-app.use((err,req,res,next)=>{
-    const {status = 500, message = "Something went wrong"} = err
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Something went wrong" } = err
     res.status(status).send(message)
 })
 
@@ -101,6 +120,6 @@ app.use((err,req,res,next)=>{
 
 
 const port = 3000
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Listening to port ${port}`)
 })
